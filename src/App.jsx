1ad8256c9f1f8e0903459f87f5de9d374ab9c33f
BRC-Thomas <thomasbaracani@gmail.com>
2023-07-06 18:55:18 +0200

    Compagnies and Slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Compagnies from "./components/Compagnies/Compagnies";
+import Residencies from "./components/Residencies/Residencies";
 import './App.css'
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Compagnies />
+      <Residencies />
     </div>
   );
 }
---
+++ src/components/Compagnies/Compagnies.jsx
@@ -0,0 +1,14 @@
+import './Compagnies.css'
+
+export default function Compagnies() {
+  return (
+    <section className='c-wrapper'>
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="./prologis.png" alt="" />
+        <img src="./tower.png" alt="" />
+        <img src="./equinix.png" alt="" />
+        <img src="./realty.png" alt="" />
+      </div>
+    </section>
+  )
+}
